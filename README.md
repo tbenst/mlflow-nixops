d35d1eb9f780e84f5c575385efa9e4d7cffd069a
tbenst <lensman@tylerbenster.com>
2020-01-09 12:59:21 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Create server running mlflow under nginx
+
+First, add secrets/basicauth.nix:
+```
+{ "user" = "password"; }
+```
+
+Now make `secrets/keyId.nix`:
+```
+"local_aws_profile"
+```
+
+Then, make `secrets/boto.cfg` (if using AWS bucket):
+```
+[Credentials]
+aws_access_key_id = XXXXXXXXXXXXXXXXXXXX
+aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+```
+
+Finally make `secrets/server_config.nix` (for Amazon Aurora mysql):
+```
+{
+  store_uri = "mysql://admin:@mlflow-instance-1.xxxxx.us-west-2.rds.amazonaws.com:3306/mlflow";
+  artifact_root = "s3://xxxxxxxx/mlflow-artifacts/";
+  hostname = "mlflow.xxxxx.com";
+}
+
+```
+
+This was tested on tbenst/nixpkgs with commit `40e11a4fd00b82805e7f647dcbd32aeaa1eeffb5`.
+**Note that mlflow-server is not yet available on unstable as of 2020/01/09 (prob will be in a couple weeks).**
+(one time):
+```
+nixops create ./mlflow-server.nix ./mlflow-server-ec2.nix -d mlflow-server-ec2
+```
+
+Then to (re)deploy with latest config:
+```
+nixops deploy -d mlflow-server-ec2
+```
---
+++ mlflow-server.nix
@@ -0,0 +1,66 @@
+{
+  # special NixOps config
+  network = {
+    description = "MLflow server";
+    enableRollback = true;
+  };
+  # end special NixOps config
+
+  # from https://gist.github.com/nh2/28bce850755cf14bd7749ea78e4238ab
+  /* boot.kernelModules = [ "tcp_bbr" ]; # faster tcp kernel support
+  # Enable BBR congestion control
+  boot.kernel.sysctl."net.ipv4.tcp_congestion_control" = "bbr"; */
+
+  mlflow-server = { config, pkgs, ... }: let
+    server_config = import ./secrets/server_config.nix;
+  in {
+    networking.firewall.enable = true;
+    # Reject instead of drop.
+    networking.firewall.rejectPackets = true;
+    networking.firewall.allowedTCPPorts = [
+      22
+      80 # nginx
+      443 # nginx
+    ];
+
+    environment.etc."boto.cfg" = {
+      source = ./secrets/boto.cfg;
+    };
+
+    environment.systemPackages = with pkgs; [
+        libmysqlclient
+        mlflow-server
+        mysql-client
+      ];
+
+    services.fail2ban.enable = true;
+
+    services.nginx = {
+      enable = true;
+      virtualHosts = {
+        "${server_config.hostname}" = {
+          locations."/".proxyPass = "http://0.0.0.0:5000/";
+          default = true;
+          basicAuth = import ./secrets/basicauth.nix;
+          forceSSL = true;
+          enableACME = true;
+        };
+      };
+    };
+
+    systemd.services.mlflowServer = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      description = "Start the mlflow server.";
+      serviceConfig = {
+        Type = "exec";
+        Restart = "on-failure";
+        RestartSec = "30";
+        StandardOutput = "/var/log/mlflow-stdout.log";
+        StandardError = "/var/log/mlflow-stderr.log";
+        ExecStart = ''${pkgs.mlflow-server}/bin/mlflow server --backend-store-uri ${server_config.store_uri} --default-artifact-root ${server_config.artifact_root} --host 0.0.0.0'';
+      };
+    };
+
+  };
+}
